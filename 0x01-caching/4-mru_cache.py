067732784e04bfda6a868f72fc3a760f4bb980c0
Chirchir-Dan <1danchirchir@gmail.com>
2024-10-30 21:06:57 +0300

    mru caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" LRUCache module
+""" MRUCache module
 """
 
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """ LRUCache class with an LRU eviction policy """
+class MRUCache(BaseCaching):
+    """ MRUCache class with an MRU eviction policy """
 
     def __init__(self):
         """ Initialize the cache """
@@ -14,29 +14,29 @@ class LRUCache(BaseCaching):
         self.usage_order = []
 
     def put(self, key, item):
-        """ Adds an item to the cache using LRU policy """
+        """ Adds an item to the cache using MRU policy """
         if key is not None and item is not None:
-            # If the key is already in cache, remove it from usage_order
-            # to update its position
+            """ If the key already exists,
+            remove it from usage_order to update its position """
             if key in self.cache_data:
                 self.usage_order.remove(key)
 
-            # Add the item to the cache and update usage order
+            # Add the item to the cache and mark it as most recently used
             self.cache_data[key] = item
             self.usage_order.append(key)
 
-            # If cache exceeds the maximum size,
-            # remove the least recently used item
+            """ If the cache exceeds the max size,
+            discard the most recently used item """
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # The least recently used item is the first item in usage_order
-                lru_key = self.usage_order.pop(0)
-                del self.cache_data[lru_key]
-                print(f"DISCARD: {lru_key}")
+                # The most recently used item is the last item in usage_order
+                mru_key = self.usage_order.pop()
+                del self.cache_data[mru_key]
+                print(f"DISCARD: {mru_key}")
 
     def get(self, key):
         """ Gets the value associated with a key in the cache """
         if key is not None and key in self.cache_data:
-            # Update usage order because this key was recently accessed
+            # Update usage order as this key is now the most recently used
             self.usage_order.remove(key)
             self.usage_order.append(key)
             return self.cache_data[key]
