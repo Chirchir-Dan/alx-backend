d6ecfef289c4bf8716ebeb6aaedca155989b1fae
Chirchir-Dan <1danchirchir@gmail.com>
2024-10-30 16:53:30 +0300

    updated last_key after each put

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,26 +11,24 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ Initialize the cache """
         super().__init__()
-        self.last_key = None
+        self.order = []
 
     def put(self, key, item):
         """ Adds an item to the cache using LIFO policy """
         if key is not None and item is not None:
-            # Add the item to the cache
+            # If key already exists, remove it to update position
+            if key in self.cache_data:
+                self.order.remove(key)
+            # Add item to cache and update order
             self.cache_data[key] = item
-
-            # Update the last added key
-            if key != self.last_key:
-                self.last_key = key
+            self.order.append(key)
 
             # Check if we need to evict an item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # Evict the last added item (LIFO)
-                del self.cache_data[self.last_key]
-                print(f"DISCARD: {self.last_key}")
-
-                # Update last_key after deletion
-                self.last_key = key
+                # Evict the most recently added item (LIFO)
+                last_key = self.order.pop(-2)  # Second-last added key for LIFO
+                del self.cache_data[last_key]
+                print(f"DISCARD: {last_key}")
 
     def get(self, key):
         """ Gets the value associated with a key in the cache """
